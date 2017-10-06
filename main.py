fa855ca79ae2ac3886ece7ffe52433dc7c462852
trisimix <fullvoidftw@gmail.com>
2017-10-06 05:53:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import discord
+import asyncio
 import sys
+import commands
 import pickle
 import os
-import commands as cmd
+import random
+import array
 #if len(sys.argv) != 2:
 #    print('Usage: python3 main.py [token]')
+
+commandz=("yes","no","hi","hi","doctor nick","Goodnight, Demo.")
 try:
     with open(os.path.join(os.path.dirname(__file__), "token.pickle"), 'rb') as file:
         token = pickle.load(file)
     if len(token) == 59:
-        print('Found saved token in stored.py, use command tokenreset to undo this.')#youll need to add the command
+        key = int(random.random() * 10000000000000000)
+        print('Found saved token in stored.py, use phrase tokenreset'+str(key), 'to undo this.')#youll need to add the command
     else:
         raise
 except:
@@ -17,10 +23,7 @@ except:
     token = input('What is your Discord bot token? (found on Discord developer page): ')
     with open(os.path.join(os.path.dirname(__file__), "token.pickle"), 'wb') as file:
         pickle.dump(token, file)
-
 client = discord.Client()
-commands = cmd.Commands(client)
-
 
 @client.event
 async def on_ready():
@@ -30,13 +33,26 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!'):
         await handle_command(message)
 
 
 async def handle_command(message):
-    command = getattr(commands, message.content.split()[0][1:], commands.ignore)
-    await client.delete_message(message)
-    await command(message)
-
+    print('Noticed: ' + message.content)
+    if message.content == 'tokenreset'+str(key):
+        await client.send_message(message.channel, 'code accepted')
+    i = 0
+    if message.author.id == message.server.me.id:
+        i = 1
+        return
+    x = 0
+    while i == 0:
+        if commandz[x] in message.content:
+            x = x + 1
+            await client.send_message(message.channel, commandz[x])
+            i = i + 1
+        else:
+            if x == len(commandz) - 2:
+                i = i + 1
+            else:
+                x = x + 2
 client.run(token)
